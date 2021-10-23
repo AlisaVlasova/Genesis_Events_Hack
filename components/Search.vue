f0a329cef18f6c5924f0d52a5fcda26e3abcc8f2
AlisaVlasova <66749919+AlisaVlasova@users.noreply.github.com>
2021-10-23 16:55:13 +0300

    Alisa (#4)
    * components files
    
    * save
    
    * eventList start
    
    * eventList event-card desktop
    
    Co-authored-by: Alisa Vlasova <alisavlasova@MacBook-Pro.local>

---
+++ components/Search.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="search">
+    <v-toolbar
+    dark
+    >
+      <v-text-field
+        hide-details
+        append-icon="mdi-magnify"
+        single-line
+      ></v-text-field>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '@/assets/scss/_vars.scss';
+
+  .search {
+    .v-toolbar {
+      background: $space-cadet;
+      box-shadow: none;
+    }
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="events">
+    <search />
     <event-list />
   </div>
 </template>

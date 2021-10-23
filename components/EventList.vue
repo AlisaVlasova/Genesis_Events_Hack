39d9d15041c0f1f9d93a05efc3c812f4553eba66
maab2198 <maab2198@gmail.com>
2021-10-23 19:57:43 +0300

    Load events data

--- components/EventList.vue
+++ components/EventList.vue
@@ -1,19 +1,19 @@
 <template>
   <ul class="events-list">
-    <li v-for="event of getEvents" :key="event" class="events-list__item">
+    <li v-for="event of events" :key="event" class="events-list__item">
       <img class="events-list__img" src="@/assets/img/img.png" alt="" />
-
+      >>>>>>> Load events data
       <div class="events-list__description">
         <h3 class="events-list__title">
-          {{ event.name }}
+          {{ event.title }}
         </h3>
         <p class="events-list__text">
-          {{ event.text }}
+          {{ event.description }}
         </p>
         <div class="events-list__bottom">
           <ul class="events-list__tags">
             <li v-for="tag of event.tags" :key="tag" class="events-list__tag">
-              {{ tag }}
+              {{ event.tag }}
             </li>
           </ul>
           <div class="events-list__date">
@@ -29,63 +29,66 @@
 import { mapGetters } from 'vuex'
 
 export default {
-  data: () => ({}),
-  computed: {
-    ...mapGetters(['getEvents']),
+  props: {
+    events: {
+      type: Array,
+      required: true,
+    },
   },
+  data: () => ({}),
 }
 </script>
 
 <style lang="scss">
 @import '@/assets/scss/_vars.scss';
-
 .events-list {
   &__item {
     display: flex;
-    padding: 24px 32px;
+    flex-direction: column;
+    padding: 24px 0;
     max-width: 100%;
+    @media (min-width: 769px) {
+      flex-direction: row;
+    }
   }
-
   &__img {
-    max-width: 40%;
-    margin-right: 32px;
+    max-width: 100%;
+    margin-bottom: 32px;
+    @media (min-width: 769px) {
+      max-width: 40%;
+      margin-right: 32px;
+      margin-bottom: 0;
+    }
   }
-
   &__description {
     display: flex;
     flex-direction: column;
   }
-
   &__title {
     margin-bottom: 16px;
     font-size: 32px;
     line-height: 100%;
     color: $tangerine;
-
     @media (min-width: 769px) {
       margin-bottom: 32px;
       font-size: 50px;
     }
   }
-
   &__text {
     color: $text;
   }
-
   &__bottom {
     display: flex;
     justify-content: space-between;
     align-items: flex-end;
     margin-top: auto;
   }
-
   &__tags {
     display: flex;
     gap: 8px;
     max-width: 50%;
     margin-right: 16px;
   }
-
   &__tag {
     background-color: $purple-navy;
     color: $text;
@@ -93,7 +96,6 @@ export default {
     border-radius: 8px;
     text-align: center;
   }
-
   &__date {
     color: $tangerine;
   }

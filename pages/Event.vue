222f253fd0ba4c5e6bc26edf6121b92faae7baed
maab2198 <maab2198@gmail.com>
2021-10-23 15:22:11 +0300

    Event page: fetch event by id

--- pages/Event.vue
+++ pages/Event.vue
@@ -1,8 +1,17 @@
 <template>
-    <h1>Event</h1>
+  <div>
+    <h1>{{ event.title }}</h1>
+    <img :src="event.img" />
+    <p>{{ event.city }}</p>
+  </div>
 </template>
 <script>
 export default {
-    
+  async asyncData({ $http, params }) {
+    const event = await $http.$get(
+      'https://kyiv-events-b93ca-default-rtdb.europe-west1.firebasedatabase.app/events/112314314.json'
+    )
+    return { event }
+  },
 }
 </script>

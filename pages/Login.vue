3136085d07014e45fe771089889186196d9983d0
kto <kto@MacBook-Pro-Ektova.local>
2021-10-24 09:24:01 +0300

    added login state

--- pages/Login.vue
+++ pages/Login.vue
@@ -39,7 +39,6 @@ export default {
             method: 'POST', 
             headers: {
               'Content-Type': 'application/json',
-              
             },
             body: JSON.stringify(this.data), // body data type must match "Content-Type" header
           }

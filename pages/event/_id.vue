c080a914d68472c2530f33d70ef126338e8f8d54
Leonid Shvab <matembit1996@gmail.com>
2021-10-24 12:21:13 +0300

    ss

--- pages/event/_id.vue
+++ pages/event/_id.vue
@@ -1,18 +1,44 @@
 <template>
-  <div v-if="event">
-    <h1>{{ event.title }}</h1>
-    <img :src="event.img" />
-    <p>{{ event.city }}</p>
-    <div><LazyCommentsList /></div>
-  </div>
+  <section v-if="event" class="event">
+    <div class="event__info">
+      <div>
+        <img :src="event.img" />
+      </div>
+      <div>
+        <h1>{{ event.title }}</h1>
+        <p>{{ event.city }}</p>
+      </div>
+    </div>
+
+    <div class="event__comments">
+      <LazyCommentsList v-if="commentsList.length > 0" :comments="commentsList" />
+      <VNoItems v-else essence="comments" />
+    </div>
+  </section>
 </template>
 <script>
 export default {
-  async asyncData({ $http, params }) {
-    const event = await $http.$get(
-      `https://kyiv-events-b93ca-default-rtdb.europe-west1.firebasedatabase.app/events/${params.id}.json`
-    )
-    return { event }
+    async asyncData({ $http, params }) {
+      const event = await $http.$get(
+        `https://kyiv-events-b93ca-default-rtdb.europe-west1.firebasedatabase.app/events/${params.id}.json`
+      )
+
+      const comments = await $http.$get(
+        `https://kyiv-events-b93ca-default-rtdb.europe-west1.firebasedatabase.app/comments/${params.id}.json`
+      )
+
+      return { event, comments }
+    },
+
+  computed: {
+    commentsList() {
+      console.log(Object.values(this.comments));
+      return Object.values(this.comments)
+    }
   },
 }
 </script>
+
+<style scoped lang="scss">
+
+</style>
